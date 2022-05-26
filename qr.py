57c9105a0c53f40448aa31f4785bcd5c492bdd03
adw97 <adwjoshi97@gmail.com>
2022-05-26 12:39:14 +0530

    changes

--- qr.py
+++ qr.py
@@ -2,5 +2,5 @@ import os
 import qrcode
 
 img = qrcode.make("https://www.youtube.com/watch?v=8JnfIa84TnU")
-img.save("solo-qr.png", "PNG")
+img.save("QRcode.png", "PNG")
 os.system("start solo-qr.png")
